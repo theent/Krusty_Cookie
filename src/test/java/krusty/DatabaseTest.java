e2029b5c770f2d12691d735fd99c0c4fa1cc87d5
theent <33178512+theent@users.noreply.github.com>
2021-03-29 19:03:28 +0200

    Prov

---
+++ src/test/java/krusty/DatabaseTest.java
@@ -0,0 +1,30 @@
+package krusty;
+
+import junit.framework.TestCase;
+
+public class DatabaseTest extends TestCase {
+
+    public void testConnect() {
+    }
+
+    public void testGetCustomers() {
+    }
+
+    public void testGetRawMaterials() {
+    }
+
+    public void testGetCookies() {
+    }
+
+    public void testGetRecipes() {
+    }
+
+    public void testGetPallets() {
+    }
+
+    public void testReset() {
+    }
+
+    public void testCreatePallet() {
+    }
+}
